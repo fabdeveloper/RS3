f0ffcf57f3c7a1c971cd4dd0b4bce2b80180936b
fabdeveloper <fabo_t@hotmail.com>
2020-12-03 21:36:04 +0100

    audit

--- src/main/java/src/dao/AbstractDao.java
+++ src/main/java/src/dao/AbstractDao.java
@@ -12,9 +12,11 @@ import javax.persistence.criteria.Root;
 
 import src.inter.IServiceLocator;
 import src.interceptors.AuditInterceptor;
+import src.interceptors.Auditor;
+import src.interceptors.DaoAuditory;
 import src.service.ServiceLocator;
 
-@Interceptors(value=AuditInterceptor.class)
+@Interceptors(AuditInterceptor.class)
 public abstract class AbstractDao<T> implements IDao<T> {
 	
 	@Inject
@@ -35,25 +37,29 @@ public abstract class AbstractDao<T> implements IDao<T> {
 		return em;
 	}
 	
-	@Interceptors(value=AuditInterceptor.class)
+	@Interceptors(AuditInterceptor.class)	
 	@Override
 	public void create(T entity){
 		getEntityManager().persist(entity);		
 	}
+	@Auditor
 	@Override
 	public void edit(T entity){
 		getEntityManager().merge(entity);
 	}
+	@Auditor
 	@Override
 	public void remove(T entity){
 		getEntityManager().remove(getEntityManager().merge(entity));
 	}
 	
+	@Auditor
 	@Override
 	public T find(Object id){
 		return getEntityManager().find(entityClass, id);		
 	}
 	
+	@Auditor
 	@Override
 	public int count(){
 		
@@ -65,6 +71,7 @@ public abstract class AbstractDao<T> implements IDao<T> {
 
 	}
 	
+	@Auditor
 	@Override
 	public List<T> getAll(){
 		
@@ -79,16 +86,19 @@ public abstract class AbstractDao<T> implements IDao<T> {
 		
 	}
 	
+	@Auditor
 	@Override
 	public T createNamedQuery(String queryname, String paramname, String paramvalue){
 		return getEntityManager().createNamedQuery(queryname, entityClass).setParameter(paramname, paramvalue).getSingleResult();
 	}
 	
+	@Auditor
 	@Override
 	public List<T> createNamedQueryListResult(String queryname, String paramname, String paramvalue){
 		return getEntityManager().createNamedQuery(queryname, entityClass).setParameter(paramname, paramvalue).getResultList();
 	}
 	
+	@Auditor
 	@Override
 	public List<T> createNamedQueryListResultIntParam(String queryname, String paramname, Integer paramvalue){
 		return getEntityManager().createNamedQuery(queryname, entityClass).setParameter(paramname, paramvalue).getResultList();
--- src/main/java/src/interceptors/AuditInterceptor.java
+++ src/main/java/src/interceptors/AuditInterceptor.java
@@ -1,38 +1,70 @@
 package src.interceptors;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Date;
-import java.util.List;
 import java.util.Map;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 
-
+@Auditor
+//@DaoAuditory
 @Interceptor
 public class AuditInterceptor {
 	
-	static Logger logger = Logger.getLogger(AuditInterceptor.class.getName());
+	private final static Logger logger = Logger.getLogger("auditInterceptor");
+
+	static{	
+		Handler consoleHandler = new ConsoleHandler();
+		Handler fileHandler = null;
+		try {
+			fileHandler = new FileHandler("auditinterceptor.log", true);
+		} catch (SecurityException | IOException e) {
+
+
+			System.out.println("AuditInterceptor - error creando logger filehandler");
+			logger.log(Level.SEVERE, "AuditInterceptor - error creando logger filehandler");
+			e.printStackTrace();
+		}
+		if(consoleHandler != null){
+			consoleHandler.setLevel(Level.ALL);
+			logger.addHandler(consoleHandler);
+		}
+		if(fileHandler != null){
+			SimpleFormatter sf = new SimpleFormatter();
+			fileHandler.setFormatter(sf);
+			fileHandler.setLevel(Level.ALL);
+			logger.addHandler(fileHandler);
+		}
+		logger.log(Level.INFO, "AuditInterceptor - logger inicializado");
+
+
+
+	}
+
 	
 	
 	@AroundInvoke
-	public Object logAudit(InvocationContext ctx){
-		
+	public Object logAudit(InvocationContext ctx){	
 		System.out.println("LOGAUDIT INTERCEPTOR - " + new Date());
 		Object target = ctx.getTarget();
 		Method method = ctx.getMethod();
 		Object[] listaParam = ctx.getParameters();
 		Map<String, Object>  contextMap = ctx.getContextData();
 		
-		
 		String log = "AUD - " + new Date() + " - ," +
 				"TARGET = " + target.toString() + ", " +
 				"METHOD = " + method.getName() + " - " +
-
 				"";
+		
 		
 		logger.log(Level.ALL, log);
 		logger.log(Level.INFO, log);
@@ -47,5 +79,8 @@ public class AuditInterceptor {
 		return null;
 		
 	}
+	
+
+
 
 }
