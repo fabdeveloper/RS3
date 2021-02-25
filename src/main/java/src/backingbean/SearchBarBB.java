b1a4dde107230cd0a6d6cbc023237afdb31cc80f
fabdeveloper <fabo_t@hotmail.com>
2021-02-25 19:53:20 +0100

    logbar

---
+++ src/main/java/src/backingbean/SearchBarBB.java
@@ -0,0 +1,38 @@
+package src.backingbean;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import src.shopping.inter.IShoppingFacade;
+
+
+@Named
+@RequestScoped
+public class SearchBarBB {
+	
+	@Inject
+	private IShoppingFacade shoppingFacade;
+	
+	public String getHomeButtonText(){
+		return shoppingFacade.getString("searchbar_homebuttontext");
+	}
+	
+	public String homeButtonAction(){
+		return shoppingFacade.getViewStateMachine().setHomeView();		
+	}
+
+	public IShoppingFacade getShoppingFacade() {
+		return shoppingFacade;
+	}
+
+	public void setShoppingFacade(IShoppingFacade shoppingFacade) {
+		this.shoppingFacade = shoppingFacade;
+	}
+	
+	
+	
+
+
+
+}
